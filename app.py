c36129f641940022c7e3830b8ccebd7519e4393e
Aravind12102 <145488819+Aravind12102@users.noreply.github.com>
2025-04-18 12:43:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def show():
     return jsonify({"response": form_result_cache["response"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8501, debug=True)
